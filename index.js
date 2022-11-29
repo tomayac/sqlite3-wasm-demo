5e6eb8001fb45abdd202540420c4f02017530a59
Thomas Steiner <tomac@google.com>
2022-11-29 06:46:02 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public', {
+  setHeaders: function (res, path, stat) {
+  res.set('Cross-Origin-Embedder-Policy', 'require-corp');
+  res.set('Cross-Origin-Opener-Policy', 'same-origin');
+}}));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
