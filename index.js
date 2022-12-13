f50f66849d7b234c5086e7d196a994d28a970359
Thomas Steiner <tomac@google.com>
2022-12-13 13:44:22 +0100

    Simpler

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.use(express.static('public', {
-  setHeaders: function (res, path, stat) {
-  res.set('Cross-Origin-Embedder-Policy', 'require-corp');
-  res.set('Cross-Origin-Opener-Policy', 'same-origin');
-}}));
+app.use(
+  express.static('public', {
+    setHeaders: function (res, path, stat) {
+      res.set('Cross-Origin-Embedder-Policy', 'require-corp');
+      res.set('Cross-Origin-Opener-Policy', 'same-origin');
+    },
+  })
+);
 
 app.listen(port, () => {
-  console.log(`SQLite Wasm demo app listening on port ${port}`)
+  console.log(`SQLite Wasm demo app listening on port ${port}`);
 });
